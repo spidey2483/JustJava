589cf01ca1827f0dc25ed8e24ab2ea67e5c49474
spidey2483 <spidey2483@gmail.com>
2016-12-02 17:23:52 -0500

    Initial commit

---
+++ app/src/main/java/com/example/jpicon/justjava/MainActivity.java
@@ -0,0 +1,72 @@
+package com.example.jpicon.justjava;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+/**
+ * This app displays an order form to order coffee.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    int quantity = 0;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * This method is called when the order button is clicked.
+     */
+    public void submitOrder(View view) {
+        int price = calculatePrice();
+        String summary = createOrderSummary();
+        String priceMessage = ("Total: $" + price);
+        displayMessage(summary + "\n" + priceMessage + "\nThank You!");
+    }
+    public void increment(View view) {
+
+        quantity = quantity + 1;
+        displayQuantity(quantity);
+    }
+    public void decrement(View view) {
+
+        quantity = quantity - 1;
+        displayQuantity(quantity);
+    }
+
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void displayQuantity(int numberOfCoffees) {
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + numberOfCoffees);
+    }
+    /**
+     * This method displays the given text on the screen.
+     */
+    private void displayMessage(String message) {
+        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
+        orderSummaryTextView.setText(message);
+    }
+
+    /**
+     * Calculates the price of the order.
+     *
+     * @return total price
+     */
+    private int calculatePrice() {
+        int price = quantity * 5;
+        return price;
+    }
+    private String createOrderSummary() {
+        String orderSummary = "Name: Javier P." + "\nQuantity: " + quantity;
+        return orderSummary;
+    }
+
+}
